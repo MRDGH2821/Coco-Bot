8def06f78f9566ded50817e850cc65a7bf702e66
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-06-03 11:37:00 +0000

    refactor(command): :recycle: directly register global commands

--- src/commands/register.rs
+++ src/commands/register.rs
@@ -2,6 +2,6 @@ use crate::{Context, Error};
 
 #[poise::command(prefix_command)]
 pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
-    poise::builtins::register_application_commands_buttons(ctx).await?;
+    poise::builtins::register_application_commands(ctx, true).await?;
     Ok(())
 }
