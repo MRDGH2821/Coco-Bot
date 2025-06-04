ffc3f08cd9ecabdc70d8c0a53cc5acc05d424991
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-06-04 08:42:24 +0000

    chore: :adhesive_bandage: overwrite all git hooks whe cocogitto installs it

--- .devcontainer/cocogitto-bootstrap.sh
+++ .devcontainer/cocogitto-bootstrap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # Bash
+mkdir -p ~/.local/share/bash-completion/completions
+touch ~/.local/share/bash-completion/completions/cog.bash-completion
+
 cog generate-completions bash >~/.local/share/bash-completion/completions/cog.bash-completion
 
-cog install-hook --all
+cog install-hook --all -o
