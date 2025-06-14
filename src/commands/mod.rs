1bb9b899f7933bff0edb5426ef7deb7b881bcc3b
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-06-14 05:40:13 +0000

    refactor: :recycle: register command on login instead of via command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -7,7 +7,6 @@ mod generate_meme;
 mod let_me_google_that;
 mod pickup_line;
 mod ping;
-mod register;
 mod say;
 mod user_info;
 mod xkcd;
@@ -27,7 +26,6 @@ pub fn all_commands() -> Vec<Command<Data, Error>> {
         let_me_google_that::let_me_google_that(),
         pickup_line::pickup_line(),
         ping::ping(),
-        register::register(),
         say::say(),
         user_info::user_info(),
         xkcd::xkcd(),
--- src/events/ready.rs
+++ src/events/ready.rs
@@ -1,4 +1,5 @@
-use poise::serenity_prelude as serenity;
+use crate::commands;
+use poise::{builtins::register_globally, serenity_prelude as serenity};
 use tracing::info;
 
 pub async fn ready(ctx: serenity::Context, ready: serenity::Ready) {
@@ -11,6 +12,16 @@ pub async fn ready(ctx: serenity::Context, ready: serenity::Ready) {
         guild_count = ready.guilds.len(),
         "Connected to {} guilds",
         ready.guilds.len()
+    );
+
+    let all_commands = commands::all_commands();
+    register_globally(&ctx.http, &all_commands)
+        .await
+        .expect("Failed to register commands globally");
+
+    info!(
+        command_count = all_commands.len(),
+        "Commands registered globally"
     );
 
     // Set bot activity/status
